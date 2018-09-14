5f777ae70a37badb49cb1cf6c71514749ab7ea95
Richard <richardvanwieringen@MacBook-Pro-van-Richard.local>
2018-09-14 14:11:36 +0200

    EndPoints and Configs

--- src/main/java/com/onboarding/onboarding/persistence/ManagerService.java
+++ src/main/java/com/onboarding/onboarding/persistence/ManagerService.java
@@ -1,5 +1,6 @@
 package com.onboarding.onboarding.persistence;
 
+import com.onboarding.onboarding.model.Manager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,4 +24,5 @@ public class ManagerService {
         Iterable <Manager> result = managerRepository.findAll();
         return result;
     }
+    
 }

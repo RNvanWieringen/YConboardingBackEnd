da51bfb6303249470ac9ac0665fc7955f7a72b56
Richard <richardvanwieringen@MacBook-Pro-van-Richard.local>
2018-09-14 14:43:52 +0200

    Add endpoints and updated YCProgramService

--- src/main/java/com/onboarding/onboarding/persistence/YCProgramService.java
+++ src/main/java/com/onboarding/onboarding/persistence/YCProgramService.java
@@ -1,4 +1,34 @@
 package com.onboarding.onboarding.persistence;
 
+
+import com.onboarding.onboarding.model.YCProgram;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class YCProgramService {
+
+    @Autowired
+    private YCProgramRepository ycProgramRepository;
+
+    public YCProgram save(YCProgram ycProgram){
+        return ycProgramRepository.save(ycProgram);
+
+
+    }
+    public YCProgram findById(Long id){
+        return  ycProgramRepository.findById(id).get();
+    }
+
+    public Iterable<YCProgram> findAll(){
+        Iterable<YCProgram> result = ycProgramRepository.findAll();
+        return result;
+    }
+
+
+
+
 }

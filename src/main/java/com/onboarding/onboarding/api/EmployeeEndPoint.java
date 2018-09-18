3c95278ab131d7347ba24b456a71f96e35c16a9c
WJJongkind <imjustacowlolz@gmail.com>
2018-09-18 13:55:59 +0200

    only required forms are downloaded now. Forms are linked to a program.

--- src/main/java/com/onboarding/onboarding/api/EmployeeEndPoint.java
+++ src/main/java/com/onboarding/onboarding/api/EmployeeEndPoint.java
@@ -31,6 +31,8 @@ public class EmployeeEndPoint {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
     public Response postEmployee(Employee employee){
+        Employee withProgram = employeeService.findById(employee.getId());
+        employee.setYcProgram(withProgram.getYcProgram());
         Employee result = employeeService.save(employee);
         return Response.accepted(result.getId()).build();
     }
